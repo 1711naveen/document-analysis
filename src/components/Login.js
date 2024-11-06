a1531cf20ab11fc5ad051ff04cc5aa06f1866c51
naveen <yn.naveen00@gmail.com>
2024-11-06 17:28:30 +0530

    Login page updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Image from 'next/image';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { useRouter } from 'next/navigation'
+import { CgProfile } from "react-icons/cg";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -10,7 +11,7 @@ const Login = () => {
   const [password, setPassword] = useState('12345');
   const router = useRouter()
 
-  const togglePasswordVisibility = () => {  
+  const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
   const handleLogin = () => {
@@ -21,49 +22,50 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center h-screen ">
-      <div className="rounded-lg shadow-lg p-10 w-4/12 bg-[#323A4F]">
+      <div className="rounded-lg shadow-lg p-10 w-4/12 bg-light-background">
         <div className="flex justify-center mb-2">
-          <Image
+          {/* <Image
             src="/path-to-your-image.png"
             alt="Logo"
             width={60}
             height={60}
             className="rounded-full"
-          />
+          /> */}
+          <CgProfile className="text-6xl text-light-black" />
         </div>
 
-        <h2 className="text-center text-2xl font-bold mb-4">Log in</h2>
+        <h2 className="text-center text-2xl font-bold mb-2">Log in</h2>
 
         <p className="text-center text-xs mb-4">
-          Don&apos;t have an account? <a href="/signup" className="">Sign up</a>
+          Don&apos;t have an account? <a href="/signup" className="underline">Sign up</a>
         </p>
 
         <div className="mb-4">
-          <label htmlFor="email" className="block text-xs">Your email</label>
+          <label htmlFor="email" className="block text-sm">Your email</label>
           <input
             id="email"
             type="email"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full p-2 border border-white outline-none rounded-lg bg-[#323A4F] text-white placeholder:text-gray-400"
+            className="w-full p-2 border border-white outline-none rounded-lg bg-light-background text-white placeholder:text-gray-400"
             required
           />
         </div>
 
         <div className="relative mb-2">
-          <label htmlFor="password" className="block text-xs">Your password</label>
+          <label htmlFor="password" className="block text-sm mb-1">Your password</label>
           <input
             type={showPassword ? "text" : "password"}
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 border border-white outline-none rounded-lg bg-[#323A4F] text-white placeholder:text-gray-400"
+            className="w-full p-2 border border-white outline-none rounded-lg bg-light-background text-white placeholder:text-gray-400"
             required
           />
           <span
             onClick={togglePasswordVisibility}
-            className="absolute -top-1 right-1 cursor-pointer"
+            className="absolute top-1 right-1 cursor-pointer"
           >
             {showPassword ? <FaEyeSlash /> : <FaEye />}
           </span>
